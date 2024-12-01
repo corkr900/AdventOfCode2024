4d52cbeda601845dd95dc80c4b5d67cc14235622
corkr900 <109830893+corkr900@users.noreply.github.com>
2024-12-01 10:31:18 -0600

    Initial commit

---
+++ Days/01/Day01.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024.Days._01 {
+	internal class Day01 {
+		public static void Do() {
+			const string inputFile = "Input/01a.txt";
+
+			string s = File.ReadAllText(inputFile);
+			Console.WriteLine(s);
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using AdventOfCode2024.Days._01;
+
+namespace AdventOfCode2024 {
+	internal class Program {
+		static void Main(string[] args) {
+
+			Day01.Do();
+
+			Console.ReadKey();
+		}
+	}
+}
